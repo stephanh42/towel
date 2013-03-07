0b9bb6bb8da1198d09650371076588d9d8073057
Stephan Houben <stephanh42@gmail.com>
2013-03-07 13:57:57 +0100

    Made setup.py work with Python2 and Python3

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from glob import glob
+from sys import version_info
 
 files = glob("src/*.cxx") + ["src/mersenne/mt19937ar.c"]
 headers = glob("src/*.h*") + ["src/mersenne/mt19937ar.h"]
@@ -13,7 +14,7 @@ setup(name="towel",
       author_email="stephanh@planet.nl",
       ext_modules=[
                     Extension("towel",files,
-                      libraries=["boost_python"],
+                      libraries=["boost_python-py%d%d" % (version_info.major, version_info.minor)],
                       depends=headers
                       ),
                     ]
